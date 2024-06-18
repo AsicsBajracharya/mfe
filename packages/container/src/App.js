a214d147668f11258ad05efa18cfa18e0804e26f
Ashish Bajracharya <ashish.bajracharya@cnspartner.com>
2024-06-18 12:41:40 +0545

    adds header to the container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 export default () =>{
     console.log('test!!!')
     return (
-        <>
-            <h1> hi there!!!!</h1>
+        <BrowserRouter>
+            <Header />
             <MarketingApp />
-        </>
+        </BrowserRouter>
        
     )
 }
